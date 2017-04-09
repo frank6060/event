4875b54386edb4b66e9c77371b94d7082b0eb92e
frank boakye yiadom <fbyiadom@gmail.com>
2017-04-09 06:52:09 -0400

    added like and dislike to our application

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+  resources :posts do
+		member do
+			get "like", to: "posts#upvote"
+			get "dislike", to: "post#downvote"
+		end
+		resources :comments
+	end
+
+	root 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
